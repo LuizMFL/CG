da0859f4e5476fe76347904930903a5e84cf5698
LuizMFL <luiz.m.franca@hotmail.com>
2023-02-10 09:53:58 -0300

    Init

---
+++ VA1/main.py
@@ -0,0 +1,9 @@
+from PIL import Image, ImageDraw
+
+class Triangulo:
+    def __init__(self, vertices:list, indice_vertice:list) -> None:
+        self.vertices = vertices
+        self.indice_vertice = indice_vertice
+
+if __name__ == '__main__':
+    pass
